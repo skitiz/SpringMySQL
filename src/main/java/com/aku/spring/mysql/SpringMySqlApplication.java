6daf8b25d823700e9c074295f5a92fd91dfd11f3
Kshitij Bantupalli <kshitij.bantupalli@hotmail.com>
2019-05-02 19:36:33 -0400

    :tada:

---
+++ src/main/java/com/aku/spring/mysql/SpringMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.aku.spring.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMySqlApplication.class, args);
+    }
+
+}
